d122344add1eb918119e459628a06ad47319331c
Kaio Ferreira <63658991-Oiak>
2020-04-16 15:12:52 -0300

    organizando imports

--- curso.projeto/src/main/java/curso/projeto/ExampleResource.java
+++ curso.projeto/src/main/java/curso/projeto/ExampleResource.java
@@ -1,12 +1,9 @@
 package curso.projeto;
 
-import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import java.util.List;
 import javax.inject.Inject;
-import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
