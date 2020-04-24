c8233f9b6415784a3b790921996bcb029fdf7c7c
Kaio Ferreira <63658991-Oiak>
2020-04-24 15:05:55 -0300

    adicionando projeto git-lab-ci

--- curso.projeto/src/main/java/curso/projeto/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package curso.projeto;
-
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import javax.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class CategoriaRepository implements PanacheRepository<Categoria> {
-
-}
